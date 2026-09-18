5316bf492c1ed5beea82892eb462686e597a22e1
shubadnanotlobb <shubadnanotlobb@gmail.com>
2026-09-18 04:35:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function performAdminLogin() {
 
     // اسم المستخدم وكلمة المرور الجديدة
     const correctUsername = "admin";
-    const correctPassword = "1234567"; 
+    const correctPassword = "12345"; 
 
     const enterUser = userInput ? userInput.value.trim() : '';
     const enterPass = passInput ? passInput.value.trim() : '';
